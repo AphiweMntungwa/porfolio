73dad4c590e8496ba18337427755775ddb432045
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-01-17 23:59:17 +0200

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./navbar.css";
 import Sidelinks from "../Navlinks/Sidelinks";
 
-function Navbar() {
-  return (
-    <nav>
-      <Sidelinks link="Profile" iconClass='far fa-user-circle' sideIcon='side-icon'/>
-      <Sidelinks link="" iconClass='' sideIcon='null side-icon'/>
-      <Sidelinks link="About" iconClass='fas fa-arrow-circle-right' sideIcon='side-icon'/>
-      <Sidelinks link="Projects" iconClass='fas fa-tasks' sideIcon='side-icon'/>
-      <Sidelinks link="Blog" iconClass='fas fa-pencil-alt' sideIcon='side-icon'/>
-    </nav>
-  );
-}
+const Navbar = () => (
+  <nav>
+    <Sidelinks
+      link="Profile"
+      iconClass="far fa-user-circle"
+      sideIcon="side-icon"
+    />
+    <Sidelinks link="" iconClass="" sideIcon="null side-icon" />
+    <Sidelinks
+      link="About"
+      iconClass="fas fa-globe-africa"
+      sideIcon="side-icon"
+    />
+    <Sidelinks link="Projects" iconClass="fas fa-tasks" sideIcon="side-icon" />
+    <Sidelinks link="Blog" iconClass="fas fa-pencil-alt" sideIcon="side-icon" />
+  </nav>
+);
 
 export default Navbar;
---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './profile.css'
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="profile">
+        <h1>Aphiwe Mntungwa</h1>
+      </div>
+    );
+  }
+}
+
+export default Profile;
