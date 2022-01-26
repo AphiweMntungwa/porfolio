3151bcbe2d9884350bdb4a72cab1368687ae6369
AphiweMntungwa <aphiwemntungwa66@gmail.com>
2022-01-26 15:32:09 +0200

    bugs

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Box from "../BoxComponent/BoxComponent";
 import Header from "./Header";
 import ProfileImage from "./ProfileImage";
